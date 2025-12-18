181f8add453adfead6f6b2be0e8ef458c775adf5
Itule Godstime <itulegodstime3546@gmail.com>
2025-12-18 04:28:45 +0100

    Fix: add headers to prevent browser caching

--- next.config.js
+++ next.config.js
@@ -3,6 +3,30 @@ const nextConfig = {
   images: {
     domains: ['is1-ssl.mzstatic.com'],
   },
+  poweredByHeader: false,
+  generateEtags: false,
+  
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate, max-age=0',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
